a72c89c9f6a049151404ef8a636584968f649c95
mcagabe19 <82843871+mcagabe19@users.noreply.github.com>
2023-02-16 16:29:44 +0300

    imma sorry

--- source/SUtil.hx
+++ source/SUtil.hx
@@ -97,14 +97,41 @@ class SUtil
 				LimeSystem.exit(1);
 			}
 		}
-		#end
+                #end
 
-		#if mobile
+                #if mobile
+                if (!FileSystem.exists(SUtil.getStorageDirectory() + 'assets') && !FileSystem.exists(SUtil.getStorageDirectory() + 'mods'))
+		{
+			Lib.application.window.alert("Whoops, seems like you didn't extract the files from the .APK!\nPlease copy the files from the .APK to\n" + SUtil.getStorageDirectory(),
+				'Error!');
+			LimeSystem.exit(1);
+		}
+		else if ((FileSystem.exists(SUtil.getStorageDirectory() + 'assets') && !FileSystem.isDirectory(SUtil.getStorageDirectory() + 'assets'))
+			&& (FileSystem.exists(SUtil.getStorageDirectory() + 'mods') && !FileSystem.isDirectory(SUtil.getStorageDirectory() + 'mods')))
+		{
+			Lib.application.window.alert("Why did you create two files called assets and mods instead of copying the folders from the .APK?, expect a crash.",
+				'Error!');
+			LimeSystem.exit(1);
+		}
+                else
+		if (!FileSystem.exists(SUtil.getStorageDirectory() + 'mods'))
+		{
+			Lib.application.window.alert("Whoops, seems like you didn't extract the assets/mods folder from the .APK!\nPlease copy the assets/mods folder from the .APK to\n" + SUtil.getStorageDirectory(),
+				'Error!');
+			LimeSystem.exit(1);
+		}
+		else if (FileSystem.exists(SUtil.getStorageDirectory() + 'mods') && !FileSystem.isDirectory(SUtil.getStorageDirectory() + 'mods'))
+		{
+			Lib.application.window.alert("Why did you create a file called mods instead of copying the mods directory from the .APK?, expect a crash.",
+				'Error!');
+			LimeSystem.exit(1);
+		}
 		if (!sys.FileSystem.exists(SUtil.getStorageDirectory()))
 		{
 			Lib.application.window.alert('Please create folder to\n' + SUtil.getStorageDirectory() + '\nPress Ok to close the app', 'Error!');
 			LimeSystem.exit(1);
 		}
+                }
 		#end
 	}
 
